3b3204726f5e8b54e411854e2bba9276736be2a3
oumaimasb <96146890+oumaimasb@users.noreply.github.com>
2021-12-31 15:39:46 +0100

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.scss";
+import Nav from "./Components/Nav";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/*navbar */}
+      <Nav />
+      <div className="test"></div>
+      {/*banner */}
+
+      {/*rows */}
+
+      {/*videos */}
+
+      {/*quick views */}
+
+      {/*footer */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./Nav.scss";
+import MenuIcon from "@material-ui/icons/Menu"; // to import Materiel UI / what we will use from MUI
+import SearchIcon from "@material-ui/icons/Search";
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Nav() {
+  const [navBlack, setNavBlack] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const transitionNav = () => {
+    window.scrollY > 100 ? setNavBlack(true) : setNavBlack(false); //to active transistion when the
+  }; //-------------------------------------------------------------- window is superior to 100vh
+  useState(() => {
+    document.addEventListener("scroll", transitionNav); // to get a black nav when we scroll down
+  });
+  console.log(navBlack);
+
+  const handleClick = () => {
+    toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
+  };
+  console.log(toggleMenu);
+
+  return (
+    <div
+      className={`nav ${
+        navBlack || toggleMenu ? "nav-black" : "nav-transparent"
+      } ${toggleMenu && "show"}`}
+    >
+      <button onClick={handleClick} className="nav-burger">
+        <MenuIcon />
+      </button>
+      <img
+        className="nav-logo"
+        src="./Images/Netflix-logo.jpg"
+        width={150}
+        alt=""
+      />
+      <nav className="nav-links">
+        <a href="/" className="nav-link">
+          Acceuil
+        </a>
+        <a href="/" className="nav-link">
+          Séries
+        </a>
+        <a href="/" className="nav-link">
+          Films
+        </a>
+      </nav>
+      <div className="nav-actions">
+        <a href="/" className="nav-action">
+          <SearchIcon />
+        </a>
+        <a href="/" className="nav-action">
+          Direct
+        </a>
+        <a href="/" className="nav-action">
+          <CardGiftcardIcon />
+        </a>
+        <a href="/" className="nav-action">
+          <NotificationsIcon />
+        </a>
+        <a href="/" className="nav-action">
+          <img src="./Images/Netflix-avatar.jpg" width={50} alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
