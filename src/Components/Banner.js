cfe4c6592ccfba12739aeefad329ad41fe93493d
oumaimasb <96146890+oumaimasb@users.noreply.github.com>
2022-01-01 20:05:20 +0100

    add comments

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./Banner.scss";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import InfoIcon from "@material-ui/icons//Info";
-import axios from "axios";
+import axios from "axios"; // fetching data with axios
 import requests from "../Config/Requests";
-import { Movie } from "@material-ui/icons";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -12,6 +11,9 @@ function Banner() {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchTrending);
+      //Math.floor return the largest integer(entier) that is less than or equal to x
+      //=>we request by this function, getting a single random background
+      //image film when someone visits/refreshes the page.
 
       setMovie(
         request.data.results[
@@ -24,17 +26,19 @@ function Banner() {
   console.log(movie);
 
   function truncateText(string, n) {
-    return String?.length > n
+    // we put this condition to avoid long text's desciption
+    return String?.length > n // so we limit the caractere strings to 100, else : No desciption
       ? string.substr(0, (n = 1)) + "..."
       : "No description";
   }
-
+  // we create this const tu put image of each movie we have in banner =>url from documentation API
+  //+path of image in console
   const bannerStyle = {
     backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
     backgroundSize: "cover",
     backgroundPositions: "center center",
   };
-
+  // The title had 3 path, we put them all;
   return (
     <header className="banner" style={bannerStyle}>
       <div className="banner-content">
