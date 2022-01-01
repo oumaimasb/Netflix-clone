1775d25740422ba87d7e3f251f45ff88c4ebfda7
oumaimasb <96146890+oumaimasb@users.noreply.github.com>
2022-01-01 16:18:52 +0100

    integrateAPI/Banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Banner.scss";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import InfoIcon from "@material-ui/icons//Info";
+import axios from "axios";
+import requests from "../Config/Requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTrending);
+
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+
+  function truncateText(string, n) {
+    return String?.length > n
+      ? string.substr(0, (n = 1)) + "..."
+      : "No description";
+  }
+
+  const bannerStyle = {
+    backgroundImage: `url("https://api.themoviedb.org/3${movie.backdrop_path}")`,
+    backgroundSize: "cover",
+    backgroundPositions: "center center",
+  };
+
   return (
-    <header className="banner">
+    <header className="banner" style={bannerStyle}>
       <div className="banner-content">
-        <h1 className="banner-title">Titre</h1>
-        <p className="banner-description">Lorem ipsum dolor sit....</p>
+        <h1 className="banner-title">
+          {movie?.title || movie?.original_title}
+        </h1>
+        <p className="banner-description">
+          {truncateText(movie?.overview, 100)}
+        </p>
         <div className="banner-buttons">
           <button className="banner-button banner-button-play">
             <PlayArrowIcon /> Lecture
